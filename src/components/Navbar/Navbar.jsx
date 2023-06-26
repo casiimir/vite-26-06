f37fab59043d93377c317a250a4a44ca6c10aaeb
Iamstefanoe <stefanoar3@gmail.com>
2023-06-26 17:34:23 +0200

    26-06-2023 StefanoAre Es1-2-3avanzato-Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <ul className="navbar__ul">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Contacts</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
